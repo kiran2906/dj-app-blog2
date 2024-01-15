bd675af123fc863854b4f34621838cd22663f4c1
Kiran Kakumani <kirankakumani@Kirans-iMac.fios-router.home>
2024-01-14 21:17:35 -0500

    forms and user accounts

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog.apps.BlogConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Upon successfull login enter the url to be directed to
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
